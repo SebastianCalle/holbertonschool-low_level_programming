acb1a7a2a007d5812ed9dc45ed72c4fbadb2a790
Juan Sebastian Calle <jusemonca@gmail.com>
2019-06-19 21:22:52 -0500

    Print message

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - print a message with new line
+*Return: 0 (success)
+*/
+int main(void)
+{
+printf("with propper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
