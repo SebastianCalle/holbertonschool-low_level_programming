4dd1b86964eb42d5234f9bdc63725273b83094f2
Juan Sebastian Calle <jusemonca@gmail.com>
2019-06-19 22:34:11 -0500

    Updates changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - print message without printf or put
- * Return : 1
+*main - print message without printf or put
+*Return: 1 of have succes
 */
 int main(void)
 {
 int l;
-l = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", l);
+l = sizeof("and that piece of art is useful\"
+- Dora Korpar, 2015-10-19");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", l);
 return (1);
 }
