94e1007b67195e63690a42a20a6d00421faf2e2d
Juan Sebastian Calle <jusemonca@gmail.com>
2019-06-19 22:30:15 -0500

    Update file with fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,8 @@
 */
 int main(void)
 {
-write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+int l;
+l = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", l);
 return (1);
 }
