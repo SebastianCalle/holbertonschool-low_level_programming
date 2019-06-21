aa8a36b26dff4581e160edabdbfba49a3c213ff4
Juan Sebastian Calle <jusemonca@gmail.com>
2019-06-20 22:50:00 -0500

    Print cha with ascci numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - prints put char with ascii numbers
+ *Return: 0
+*/
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		if (n < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
