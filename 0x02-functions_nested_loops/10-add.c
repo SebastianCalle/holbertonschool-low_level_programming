e1490e314d44e4d2d8c6f37ec4d949b32c94678d
Juan Sebastian Calle <jusemonca@gmail.com>
2019-06-24 21:34:21 -0500

    Add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * add - add two integers
+ * @i: integer one
+ * @j: integer two
+ * Return: the value
+ */
+int add(int i, int j)
+{
+	int m;
+	m = i + j;
+	return(m);
+}
