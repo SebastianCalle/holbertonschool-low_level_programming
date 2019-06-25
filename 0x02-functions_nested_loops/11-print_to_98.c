484ee29ff4a3730031e8178e261b86219e5f88eb
Juan Sebastian Calle <jusemonca@gmail.com>
2019-06-24 21:51:31 -0500

    From n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * print_to_98 - print the numberso from n to 98
+ */
+void print_to_98(int n)
+{
+	int m = 98;
+
+	if (n == m)
+		printf("%d", m);
+	else
+	{
+
+		while(n < m)
+		{
+			printf("%d, ", n);
+			n++;
+			if (n == m)
+				printf("%d", n);
+		}
+
+		while(n > m)
+		{
+			printf("%d, ", n);
+			n--;
+			if (n == m)
+				printf("%d", n);
+		}
+		
+	}
+	printf("\n");
+}
