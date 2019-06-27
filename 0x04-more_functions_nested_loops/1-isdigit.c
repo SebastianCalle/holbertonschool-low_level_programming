f47b20cd4038a04a9bdce7c57224c1d645d5cdb8
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-06-27 15:08:28 +0000

    Multiplication of two integers

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _isupper - Print if a character is uppercase
+ * Return: 0
+ * @c: input a character
+ */
+int _isdigit(int c)
+{
+	int a;
+
+	if (c >= 48 && c <= 57)
+		a = 1;
+	else
+		a = 0;
+
+	return (a);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,9 @@
+#include "holberton.h"
+/**
+ * mul - print the multiplies of two ingeres
+ * Return: return the multiplication
+ */
+int mul(int a, int b)
+{
+	return a * b;
+}
