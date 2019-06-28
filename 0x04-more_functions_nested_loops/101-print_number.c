71b037d2190e57662c6664556d84b041adf1dbf7
Juan Sebastian Calle <jusemonca@gmail.com>
2019-06-27 19:49:58 -0500

    Print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,13 +9,8 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		if ((n / 10) < 0)
-		{
 			_putchar('-');
-			print_number(m);
-
-		}
-		_putchar ((m % 10) + '0');
+			m = m * -1;
 	}
 
 	if ((m / 10) > 0)
