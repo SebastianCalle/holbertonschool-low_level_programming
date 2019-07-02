6614f378eff53e10654b261572126e2012661bd5
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-07-02 15:06:53 +0000

    Fix checks puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,13 +1,28 @@
 #include "holberton.h"
 /**
+ * _sterlen - give the len of string
+ * @s: parameter
+ * Return: return len
+ */
+int _sterlen(char *s)
+{
+	int i, count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	count++;
+
+	return (count);
+}
+/**
  * puts2 - print out of 2 a string
  * @str: pointer to string
  */
 void puts2(char *str)
 {
-	int i;
+	int i, len;
 
-	for (i = 0; str[i] != '\0'; i++)
+	len = _sterlen(str);
+	for (i = 0; i < len ; i++)
 	{
 		if (str[i] % 2 == 0)
 			_putchar(str[i]);
