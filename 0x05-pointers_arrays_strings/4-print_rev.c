97f1616661bed641601295cb9537bb42337a4f91
Juan Sebastian Calle <jusemonca@gmail.com>
2019-07-01 20:34:34 -0500

    Reverse string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,10 +5,18 @@
  */
 void print_rev(char *s)
 {
-	int i;
+	int i, start, end;
+	int count = 0;
+	char rev[1000];
 
 	for (i = 0; s[i] != '\0'; i++)
-		_putchar(s[i]);
-	
+		count++;
+	end = count - 1;
+	for (start = 0; start < count; start++)
+	{
+		rev[start] = s[end];
+		end--;
+		_putchar(rev[start]);
+	}
 	_putchar('\n');
 }
