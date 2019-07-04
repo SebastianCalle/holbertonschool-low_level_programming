9d836c32f060344863ce6569961397f8edf34719
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-07-04 15:59:36 +0000

    Concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * _strlen - print the size of a string
+ * @s: char parameter
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int i, c;
+
+	for (i = 0; s[i] != '\0'; i++)
+		c++;
+	return (c);
+}
+/**
+ * _strcat - concaenate two strings
+ * @dest: parameter dest
+ * @src: parameter source
+ */
+char *_strcat(char* dest, char *src)
+{
+	int i,j;
+	
+	i = _strlen(dest);
+
+	for ( j = 0; src[j] != '\0'; j++)
+	{
+		dest[i+j] = src[j];
+	}
+	dest[i+j] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# More Pointers, strings and arrays
+
+- Learn more about the how to use pointers strings and arrays
