0932eb1720f3eb6e11c371148668d6a31b968362
Juan Sebastian Calle <jusemonca@gmail.com>
2019-07-04 21:44:27 -0500

    Fix checkers

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,9 +1,11 @@
 #include "holberton.h"
+
 /**
- * rot13 - encode a string using rot13
- * @s: revice a string pointer
- * Return: return string encode
+ * rot13 - conver string o rot13
+ * @s: string
+ * Return: string encode
  */
+
 char *rot13(char *s)
 {
 	int i;
@@ -11,15 +13,15 @@ char *rot13(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if ((s[i] >= 'a' && s[i] <= 'm') ||
-			(s[i] >= 'A' && s[i] <= 'z'))
+			(s[i] >= 'A' && s[i] <= 'M'))
 		{
-			s[i] = (s[i] + 13);
+			s[i] = s[i] + 13;
 		}
 		else
 			while ((s[i] >= 'n' && s[i] <= 'z') ||
-				(s[i] >= 'N' && s[i] <= 'Z'))
+			      (s[i] >= 'N' && s[i] <= 'Z'))
 			{
-				s[i] = (s[i] - 13);
+				s[i] = s[i] - 13;
 			}
 	}
 	return (s);
