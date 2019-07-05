fa1116541f8ede590af5430aa4f7345ffc02f815
Juan Sebastian Calle <jusemonca@gmail.com>
2019-07-04 21:47:52 -0500

    Update file 8

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -5,24 +5,25 @@
  * @s: string
  * Return: string encode
  */
-
 char *rot13(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	i = 0;
+	while (s[i] != '\0')
 	{
-		if ((s[i] >= 'a' && s[i] <= 'm') ||
-			(s[i] >= 'A' && s[i] <= 'M'))
+		if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
 		{
-			s[i] = s[i] + 13;
+			s[i] = (s[i] + 13);
 		}
 		else
 			while ((s[i] >= 'n' && s[i] <= 'z') ||
 			      (s[i] >= 'N' && s[i] <= 'Z'))
 			{
-				s[i] = s[i] - 13;
+				s[i] = (s[i] - 13);
 			}
+		i++;
 	}
 	return (s);
 }
+
