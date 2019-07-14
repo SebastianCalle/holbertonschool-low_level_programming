464a32957094c6404fe4def5e5ee81dadf975135
Juan Sebastian Calle <jusemonca@gmail.com>
2019-07-14 18:36:38 -0500

    Change file 100 for fix checkers

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,25 +8,35 @@
  */
 int main(int argc, char *argv[])
 {
-	int coins[] = {1, 2, 5, 10, 25};
-	int i, j, money;
+	int n = 0, c = 0;
 
-	j = 5;
-	i = 0;
 	if (argc == 2)
 	{
-		money = atoi(argv[1]);
-		while (money > 0)
+		n = atoi(argv[1]);
+		if (n > 0)
 		{
-			if (money >= coins[j])
+			for (; n >= 25; c++)
 			{
-				money -= coins[j];
-				i++;
+				n = n - 25;
 			}
-			else
-				j--;
+			for (; n >= 10; c++)
+			{
+				n = n - 10;
+			}
+			for (; n >= 5; c++)
+			{
+				n = n - 5;
+			}
+			for (; n >= 2; c++)
+			{
+				n = n - 2;
+			}
+			for (; n >= 1; c++)
+			{
+				n = n - 1;
+			}
 		}
-		printf("%d\n", i);
+		printf("%d\n", c);
 		return (0);
 	}
 	else
