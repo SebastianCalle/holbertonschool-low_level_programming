093b3863039e687c4fd11966bf2dca75c88a57d1
Juan Sebastian Calle <jusemonca@gmail.com>
2019-07-14 18:11:25 -0500

    Print the minimum number of coins

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the minimu number of coins
+ * @argc: number of arguments
+ * @argv: array arguments
+ * Return: 0 is is succesful or 1 is not
+ */
+int main(int argc, char *argv[])
+{
+	int coins[]={1, 2, 5, 10, 25};
+	int i, j, money;
+
+	j = 5;
+	i = 0;
+	if (argc == 1 || argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (argc == 2)
+	{
+		money = atoi(argv[1]);
+		while (money > 0)
+		{
+			if (money >= coins[j])
+			{
+				money -= coins[j];
+				i++;
+			}
+			else
+				j--;
+		}
+		printf("%d\n", i);
+	}
+	return (0);
+}
