aae415dc680053ac7f3394139e746fdd976cdf23
Juan Sebastian Calle <jusemonca@gmail.com>
2019-07-14 00:40:45 -0500

    Fix the checkers file 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - print number of arguments
+ * main - print numfer of arguments
  * @argc: coutn
  * @argv: array
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int res, i, f;
+	int res, f, i;
 
 	res = 0;
 	f = 1;
 	if (argc > 1)
 	{
-		for (i = 1; argv[i]; i++)
+		for (i = 1; i < argc; i++)
 		{
 			if (*(argv[i]) != '0' && atoi(argv[i]) == 0)
 				f = 0;
@@ -28,6 +28,5 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-
 	return (0);
 }
