ec355803517ce641de88590af071d232ce870417
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-07-16 19:06:32 +0000

    Function that concatenates two string

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,75 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _strlen - print the size of a string
+ * @s: char parameter
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+	}
+	return (i);
+}
+/**
+ * _strcat - concaenate two strings
+ * @dest: parameter dest
+ * @src: parameter source
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = _strlen(dest);
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
+/**
+ * _strcpy - copie a string
+ * @dest: destine of copy
+ * @src: source of copy
+ * Return: return copie
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: string concatenate
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *str1;
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		s1 = " ";
+		s2 = " ";
+	}
+	str1 = malloc(_strlen(s1) + _strlen(s2));
+	if (str1 == NULL)
+		return (NULL);
+	_strcpy(str1, s1);
+	if (str1 == NULL)
+		return (NULL);
+	_strcat(str1, s2);
+	return (str1);
+
+}
