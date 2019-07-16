c1a152a5fad36ed2cbc10ba323280d1c3a914532
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-07-16 19:22:11 +0000

    Fix checkers file 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -59,9 +59,9 @@ char *str_concat(char *s1, char *s2)
 	char *str1;
 
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
 	if (s2 == NULL)
-		s2 = " ";
+		s2 = "";
 	str1 = malloc(_strlen(s1) + _strlen(s2) + 1);
 	if (str1 == NULL)
 		return (NULL);
