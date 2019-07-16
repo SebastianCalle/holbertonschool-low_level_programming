e438905c3c346b5359971d7e77aa3f3896a6115d
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-07-16 17:43:50 +0000

    Fix checkers betty and change functions

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,32 @@
 #include "holberton.h"
 #include <stdlib.h>
-#include <string.h>
+/**
+ * _strlen - retruns the length of a string
+ * @s: string parameter
+ * Return: the length
+ */
+int _strlen(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (_strlen(s + 1) + 1);
+}
+/**
+ * _strcpy - copie a string
+ * @dest: destine of copy
+ * @src: source of copy
+ * Return: return copie
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
 /**
  * _strdup - return a pointer to a newly allocate space in memory
  * @str: string parameter
@@ -10,10 +36,10 @@ char *_strdup(char *str)
 {
 	char *ns;
 
-	ns = malloc(sizeof(str) * strlen(str));
+	ns = malloc(sizeof(str) * _strlen(str));
 	if (str == NULL)
 		return (NULL);
-	strcpy(ns, str);
+	_strcpy(ns, str);
 	return (ns);
 	free(ns);
 }
