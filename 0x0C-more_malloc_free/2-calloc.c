85834dce547db465ccbb8da76926739986b40d28
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-07-18 21:00:29 +0000

    allocates memory for an array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,60 +1,23 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
- * _strlen - print the size of a string
- * @s: char parameter
- * Return: 0
- */
-int _strlen(char *s)
-{
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-
-	}
-	return (i);
-}
-/**
- * _strcat - concaenate two strings
- * @dest: parameter dest
- * @src: parameter source
- * Return: dest
- */
-char *_strcat(char *dest, char *src)
-{
-	int i, j;
-
-	i = _strlen(dest);
-	for (j = 0; src[j] != '\0'; j++)
-	{
-		dest[i + j] = src[j];
-	}
-	dest[i + j] = '\0';
-	return (dest);
-}
-/**
- * _strcpy - copie a string
- * @dest: destine of copy
- * @src: source of copy
- * Return: return copie
- */
-char *_strcpy(char *dest, char *src)
-{
-	int i;
-
-	for (i = 0; src[i] != '\0'; i++)
-		dest[i] = src[i];
-	dest[i] = '\0';
-	return (dest);
-}
-/**
  * _calloc - allocates memory for an array
  * @nmemb: first parameter
  * @size: size of array
- * Retrun: memory for an array
+ * Return: memory for an array
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	unsigned int i;
+	char *s;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	s = malloc(nmemb * size + 1);
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+		s[i] = 0;
+	return (s);
 
 }
