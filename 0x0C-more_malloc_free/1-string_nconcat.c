d4ab76c704014dca142e2ce9b737354787b43885
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-07-18 18:18:53 +0000

    Fix checkers with other functions

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,51 +31,32 @@ char *_strcpy(char *dest, char *src)
 	return (dest);
 }
 /**
+ * _strncat - function to concatnate strings with n iytes
+ * @dest: destinition for concatnation
+ * @src: source of string
+ * @n: int type for size of iyte
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int len;
+	int i;
+
+	len = _strlen(dest);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[len + i] = src[i];
+	return (dest);
+}
+/**
  * _strncpy - copies a string
  * @dest: - parameter destine
  * @src: - parameter source
  * @n: parameter integer
  * Return: dest
  */
-char *_strncpy(char *dest, char *src, int n)
-{
-	int i;
-
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-	for ( ; i < n; i++)
-		dest[i] = '\0';
-	return (dest);
-}
-/**
- * _strcat - concaenate two strings
- * @dest: parameter dest
- * @src: parameter source
- * Return: dest
- */
-char *_strcat(char *dest, char *src)
-{
-	int i, j;
-
-	i = _strlen(dest);
-	for (j = 0; src[j] != '\0'; j++)
-	{
-		dest[i + j] = src[j];
-	}
-	dest[i + j] = '\0';
-	return (dest);
-}
-/**
- * string_nconcat - concatenates two strings
- * @s1: string 1
- * @s2: string 2
- * @n: size of bits
- * Return: string concatenate
- */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ns;
-	char *ns2;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -83,15 +64,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	if ((int)n > _strlen(s2))
 		n = _strlen(s2);
-	ns2 = malloc(n + 1);
-	if (ns2 == NULL)
-		return (NULL);
-	_strncpy(ns2, s2, n);
-	ns = malloc(_strlen(s1) + _strlen(ns2) + 1);
+	ns = malloc(_strlen(s1) + _strlen(s2) + 1);
 	if (ns == NULL)
 		return (NULL);
 	_strcpy(ns, s1);
-	_strcat(ns, ns2);
+	_strncat(ns, s2, n);
 	return (ns);
 
 }
