745d661c9e16cb7a9d4f0a15d114a1bf61008cbe
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-07-22 21:07:17 +0000

    Update files task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+/**
+ * new_dog - create a new dog
+ * @name: name of dog
+ * @age: age of new dog
+ * @owner: owner of new dog
+ * Return: NULL if function fails
+ */
+void new_dog(char *name, float age, chat *owner)
+{
+	
+}
