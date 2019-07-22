87c57620042f447f06e05965dddd1039260c4710
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-07-22 19:40:13 +0000

    Fix chekers file dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,16 @@
-struct dog{
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - include name age and owner
+ * @name: the name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
