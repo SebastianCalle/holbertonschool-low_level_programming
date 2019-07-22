d02b1fd6b64faccdedbaab0b2012c1da5d98b380
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-07-22 16:50:33 +0000

    Print hte name of the file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define NAME __FILE__
+/**
+ * main - program that print the name file
+ * Return: 0 if is succesfull
+ */
+int main(void)
+{
+	printf("%s\n", NAME);
+	return (0);
+}
