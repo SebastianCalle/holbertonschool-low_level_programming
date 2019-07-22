b5dd855f86b446ae636b26aeb381abd7d6f266f5
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-07-22 20:37:04 +0000

    Fix chekers file 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,17 +8,9 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		if (d == NULL)
-			printf(" ");
-		if (d->name == NULL)
-			printf("Name: (nil)");
-		if (d->age == 0)
-			printf("Age: (nil)");
-		if (d->owner == NULL)
-			printf("Owner: (nil)");
-		printf("Name: %s\n", d->name);
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
 		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 
 }
