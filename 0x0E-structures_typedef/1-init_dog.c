4dd7cd9ac96c75fa69a2cdbc212c1a85abfe0865
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-07-22 19:54:40 +0000

    Init dog structure

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * init_dog - init dog structue
+ *@d: structur
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
