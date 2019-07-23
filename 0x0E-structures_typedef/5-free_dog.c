47e9c4c115e9ed38337d0ccebbc39d6366841690
Juan Sebastian Calle <jusemonca@gmail.com>
2019-07-22 22:00:16 -0500

    Free dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - function that frees dogs
+ * @d: structur of dog
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
