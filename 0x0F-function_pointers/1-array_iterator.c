44bbbff482bcc7fc6c7528d1ff6510158dd8e0b1
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-07-24 17:44:27 +0000

    Fix chekers output file 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,11 +7,9 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	void (*ptr)(int);
 	int i;
 
-	ptr = action;
 
 	for (i = 0; i < (int)size ; i++)
-		(*ptr)(array[i]);
+		action(array[i]);
 }
