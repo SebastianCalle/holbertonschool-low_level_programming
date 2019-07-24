84cc190cf9e33f231d4c628247501753b7de4c0d
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-07-24 17:25:52 +0000

    Fix again chekers file 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
-#include "function_pointers.h"
-#include <stdlib.h>
+#include <stdio.h>
 /**
  * print_name - print a name
  * @name: name to print
@@ -9,5 +8,5 @@ void print_name(char *name, void (*f)(char *))
 {
 	void (*ptr)(char *);
 	ptr = f;
-	ptr(name);
+	(*ptr)(name);
 }
