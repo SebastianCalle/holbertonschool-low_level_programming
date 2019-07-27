febfae68aa14b9066a86ac65eaffababe88be26e
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-07-27 17:43:37 +0000

    Fix chekers betty file 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,8 +15,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(numbers, int));
-		if (separator != NULL && i != n -1)
-				printf("%s", separator);
+		if (separator != NULL && i != n -  1)
+			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(numbers);
