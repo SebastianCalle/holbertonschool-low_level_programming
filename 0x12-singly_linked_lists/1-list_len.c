60aedfa79eac18efa5e0de2ab5dc2eff09320317
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-08-01 20:50:03 +0000

    Update file 1-list_len.c and 1-main.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * print_len - print elements of list_t
+ * @h: list structure pointer
+ * Return: size of node
+ */
+size_t list_len(const list_t *h)
+{
+	size_t c = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			h = h->next;
+			c++;
+		}
+		h = h->next;
+		c++;
+	}
+
+	return (c);
+}
