9f6116bcb6f057b353a2066b04f608ff3113841b
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-08-01 23:57:18 +0000

    Update files task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - free a list_t list
+ * @head: structure
+ */
+void free_list(list_t *head)
+{
+	list_t *aux;
+
+	while(head)
+	{
+		aux = head;
+		head = head->next;
+		free(aux->str);
+		free(aux);
+	}
+}
