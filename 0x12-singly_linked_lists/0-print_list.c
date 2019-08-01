084ee3b55e95fee544f241669c9d4129dad709b2
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-08-01 20:25:49 +0000

    Fix cheker 7 file 0-print

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,14 +8,14 @@ size_t print_list(const list_t *h)
 {
 	size_t c = 0;
 
-	if (h->str == NULL)
+	while (h != NULL)
 	{
-		printf("[0] (nil)\n");
-		h = h->next;
-		c++;
-	}
-	while (h != '\0')
-	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+			c++;
+		}
 		printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		c++;
