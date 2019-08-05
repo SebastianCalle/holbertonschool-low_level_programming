f1863e3f396ab705614ddae26b86f68b28379d14
Juan Sebastian Calle <jusemonca@gmail.com>
2019-08-05 12:55:00 -0500

    Update files task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint2 - free a listint_t
+ * @head: pointer to pointer to structure
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *aux = *head;
+
+	while(aux)
+	{
+		aux= aux->next;
+		free(*head);
+		*head = aux;
+	}
+}
