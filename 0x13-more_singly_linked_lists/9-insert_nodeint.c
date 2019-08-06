296cdf8583dfd07793a400e63bb15b59ba8e6b3e
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-08-06 20:44:58 +0000

    Fix checkers file 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -7,13 +7,14 @@
  * Return: new node o null
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
-{
-	unsigned int i = 0;
+{	unsigned int i = 0;
 	listint_t *aux;
 	listint_t *new;
 
+	if (head == NULL && *head == NULL)
+		return (NULL);
 	new = malloc(sizeof(listint_t));
-	if (new == NULL || (head == NULL && *head == NULL)) 
+	if (new == NULL)
 		return (NULL);
 	aux = *head;
 	while (aux->next)
