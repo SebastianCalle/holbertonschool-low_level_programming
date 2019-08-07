b5be7a652b5deb21daa216c65cad94f4cd936617
Juan Sebastian Calle <jusemonca@gmail.com>
2019-08-07 16:32:07 -0500

    Update files task 102

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * free_listint_safe - free a list
+ * @h: Node to free
+ * Return: the size of the list;
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t i = 0;
+	listint_t *aux;
+
+	if (h == NULL && *h == NULL)
+		return (0);
+	aux = *h;
+	while(*h)
+	{
+		aux = *h;
+		*h = (*h)->next;
+		i++;
+		free(aux);
+
+		if (aux <= *h)
+		{
+			*h = NULL;
+			return (i);
+		}
+	}
+	*h = NULL;
+	return (i);
+}
