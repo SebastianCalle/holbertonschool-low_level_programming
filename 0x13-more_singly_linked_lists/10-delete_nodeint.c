bb1baaa320c24832e1255fcc5113c09bbbbb6b5d
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-08-07 00:31:15 +0000

    UPdate files task 10 and fix betty file 9

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -2,32 +2,34 @@
 /**
  * delete_nodeint_at_index - deletes the node at index of a listint_t
  * @head: pointer to pointer of a structure
- * @idx: index to delete
+ * @index: index to delete
  * Return: 1 if it succesful or -1 if it failed
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i = 0;
-	listint_t *aux = *head;
+	listint_t *aux;
+	listint_t *del;
 
-	if (*head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
-	while (aux->next)
-	{
-		aux = aux->next;
-		i++;
-	}
-	i++;
-	if (index <= i)
+	if (index == 0)
 	{
 		aux = *head;
-		for (i = 1; i < index; i++)
-			aux = aux->next;
-		aux->next = aux->next;
-		free (aux);
+		*head = aux->next;
+		free(aux);
+		return (1);
 	}
-	else
-		return (-1);
-
+	del = *head;
+	for (i = 0; i < (index - 1); i++)
+	{
+		del = del->next;
+		if (del == NULL)
+			return (-1);
+	}
+	aux = del->next;
+	del->next = aux->next;
+	free(aux);
 	return (1);
+
 }
