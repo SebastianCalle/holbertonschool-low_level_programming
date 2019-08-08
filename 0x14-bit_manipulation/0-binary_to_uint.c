acf426ff9b1b37a96556f6539d4c6b67e67b321a
Juan Sebastian Calle <jusemonca@gmail.com>
2019-08-08 12:23:52 -0500

    Fix checkers files task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -24,6 +24,8 @@ unsigned int binary_to_uint(const char *b)
 	int num[100];
 	int i, j = 0, convert = 0, n = 0;
 
+	if (b == NULL)
+		return (0);
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] >= 48 && b[i] <= 57)
