91556342db1533f415b470569d73da83cbfd7d9c
Juan Sebastian Calle <jusemonca@gmail.com>
2019-08-08 17:34:32 -0500

    update to 64 bits file 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,16 +5,17 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int i = 1 << 31;
+	unsigned long i = 1;
 	int f = 0, j;
 
-	for (j = 0; j < 32; j++)
+	i = i << 63;
+	for (j = 0; j < 64; j++)
 	{
 		if (f == 0)
 		{
 			if ((n & i) == 0)
 			{
-				if (j == 31)
+				if (j == 63)
 					_putchar('0');
 				i = i >> 1;
 				continue;
