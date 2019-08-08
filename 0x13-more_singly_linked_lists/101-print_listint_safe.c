baaed99a15291a840c33a35150486fc98a567428
Juan Sebastian Calle <jusemonca@gmail.com>
2019-08-07 21:57:10 -0500

    Update files task 13

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,20 +7,24 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	const listint_t *aux = NULL, *aux2 = NULL;
-	size_t i = 0;
+	size_t i = 0, j;
 
 	aux = head;
 	while (aux != NULL)
 	{
-		aux2 = aux;
-		aux = aux->next;
+		aux2 = head;
 		i++;
-		printf("[%p] %d\n", (void *)aux2, aux2->n);
+		printf("[%p] %d\n", (void *)aux, aux->n);
+		aux = aux->next;
 
-		if (aux2 <= aux)
+		for (j = 0; j < i; j++)
 		{
-			printf("-> [%p] %d\n", (void *)aux, aux->n);
-			return (i);
+			if (aux2 <= aux)
+			{
+				printf("-> [%p] %d\n", (void *)aux, aux->n);
+				return (i);
+			}
+			aux2 = aux2->next;
 		}
 		if (head == NULL)
 			exit(98);
