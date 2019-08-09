9eb2f85b54c64d6cfd3d3eb7c07ecae2a35863ba
Juan Sebastian Calle <jusemonca@gmail.com>
2019-08-08 20:48:29 -0500

    Fix checkers and change logic

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
  * get_bit -get the value of a bit at a given index
  * @n: number to convert
@@ -7,41 +8,14 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long i = 1;
-	int f = 0, j, num[64];
-	unsigned int c = 0;
+	unsigned long int i = 1;
 
-	i = i << 63;
-	for (j = 0; j < 64; j++)
-	{
-		if (f == 0)
-		{
-			if ((n & i) == 0)
-			{
-				if (j == 63)
-					num[j] = 0;
-				i = i >> 1;
-				continue;
-			}
-		}
-		if ((n & i) != 0)
-		{
-			f = 1;
-			num[j] = 1;
-		}
-		else
-			num[j] = 0;
-		i = i >> 1;
-	}
-	j = 63;
-	while (c < (index + 1))
-	{
-		if (c == index)
-			return (num[j]);
-		else if (c > index)
-			return (-1);
-		c++;
-		j--;
-	}
-	return (0);
+	if (index > 63)
+		return (-1);
+	i = i << index;
+	if (i & n)
+		return (1);
+	else
+		return (0);
+
 }
