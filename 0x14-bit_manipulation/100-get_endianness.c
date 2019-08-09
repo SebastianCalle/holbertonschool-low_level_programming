7fb86bd975c57ef4bbb3e4c5c6f55df51a60da13
Juan Sebastian Calle <jusemonca@gmail.com>
2019-08-08 22:10:08 -0500

    Update files task 6

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * get_endianness - checks the endianness
+ * Return: 0 if is big endian 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned long int i = 1;
+
+	if (i<<63)
+		return (1);
+	else
+		return (0);
+}
