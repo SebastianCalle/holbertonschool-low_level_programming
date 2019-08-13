dfb8080da14485d1326300b64fdfdf5dbf9f4d7e
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-08-13 18:52:55 +0000

    Update files task 0

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+#include <stdlib.h>
+#include<fcntl.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
