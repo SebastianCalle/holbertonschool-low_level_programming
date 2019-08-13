220c3b8acf0ab3d5066128273d8bd22e17428dca
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-08-13 18:44:12 +0000

    Update files task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * read_textfile - reads a text file and prints it to the POSIX
+ * @filename: file to read
+ * @letters: size of letters
+ * Return: the size of letters:
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t sz;
+	int fd;
+	char *c;
+
+	c = (char *) calloc(1024, sizeof(char));
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd < 0)
+		return (0);
+	sz = read(fd, c, letters);
+	printf("%s\n", c);
+	close(fd);
+	return (sz);
+}
---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include<stdio.h> 
+#include<string.h> 
+#include<unistd.h> 
+#include<fcntl.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
