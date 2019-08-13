bc5e5c99b5e506cdf98cd109c95422a887f4f060
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-08-13 18:50:28 +0000

    Delete files

--- 0x15-file_io/holberton.h
+++
@@ -1,9 +0,0 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-#include<stdio.h> 
-#include<string.h> 
-#include<unistd.h> 
-#include<fcntl.h>
-ssize_t read_textfile(const char *filename, size_t letters);
-
-#endif
