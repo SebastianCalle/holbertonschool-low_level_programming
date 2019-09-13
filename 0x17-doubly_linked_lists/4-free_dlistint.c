904183f0c9cc155b2ce2a8da461bb30f14a71347
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-09-13 22:38:11 +0000

    [Feature]: Create files task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_dlistint - free a list
+ * @head: list to free
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *aux;
+
+	while(head)
+	{
+		aux = head;
+		head = head->next;
+		free(aux);
+	}
+}
