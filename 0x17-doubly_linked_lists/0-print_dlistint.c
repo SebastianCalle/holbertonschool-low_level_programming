4278ab5e69250f4ae0502ef614ce88e1726ad066
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-09-13 19:28:43 +0000

    [Feature]: Create file task 0 and README.md

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *aux, *aux2;
+	size_t n = 0;
+
+	if (h == NULL)
+		return (0);
+	aux = h->prev;
+	if (aux == NULL)
+	{
+		aux2 = h;
+		while(aux2)
+		{
+			printf("%d\n", aux2->n);
+			aux2 = aux2->next;
+			n++;
+		}
+	}
+	else
+	{
+		while(aux)
+		{
+			aux = aux->prev;
+		}
+		aux2 = aux;
+		while(aux2)
+		{
+			printf("%d\n", aux2->n);
+			aux2 = aux2->next;
+			n++;
+		}
+	}
+	return (n);
+}
