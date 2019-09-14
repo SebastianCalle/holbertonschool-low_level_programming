8b7ff72c7c9bd6fb0a2f5f7cb5f3414bb5b8e9b7
Juan Sebastian Calle <jusemonca@gmail.com>
2019-09-14 12:38:52 -0500

    [Fix]: Change the logic

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,8 +13,13 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	new->next = *head;
 	new->prev = NULL;
+	if (*head != NULL)
+	{
+		while((*head)->prev != NULL)
+			*head = (*head)->prev;
+	}
+	new->next = *head;
 	*head = new;
 	return (new);
 }
