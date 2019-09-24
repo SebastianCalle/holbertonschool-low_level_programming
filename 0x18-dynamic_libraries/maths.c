efafd200b56fcfa07307b264b290b31c62f3677c
Juan Sebastian Calle <jusemonca@gmail.com>
2019-09-24 10:03:45 -0500

    [update]: Update some files

---
+++ 0x18-dynamic_libraries/maths.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * add - function that sum two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: the sum of both numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - function that rest two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: the sub of both numbers
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - function that multiplicate two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: the multiplicate both numbers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - function that divide two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: the div of both numbers
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - function that mod two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: the mod of both numbers
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
