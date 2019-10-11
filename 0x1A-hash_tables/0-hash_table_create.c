eee6bebb7109cc182bd60ed9c01d4d0e8eed3c3f
Juan Sebastian Calle <jusemonca@gmail.com>
2019-10-11 10:51:19 -0500

    [Fix]: add new condition if size is 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,15 +1,18 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_t - Function that creates a hash table
+ * hash_table_create - Function that creates a hash table
  * @size: size of array
  * Return: a pointer to a newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned int i;
-	hash_table_t *new = malloc(sizeof(hash_table_t));
+	hash_table_t *new;
 
+	if (size == 0)
+		return (NULL);
+	new =  = malloc(sizeof(hash_table_t));
 	if (!new)
 		return (NULL);
 	new->size = size;
