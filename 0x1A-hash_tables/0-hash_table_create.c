22e4e9866cc65872527b8ce58cb176569638db0f
Juan Sebastian Calle <jusemonca@gmail.com>
2019-10-11 10:52:18 -0500

    [Fix]: add new condition if size is 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,7 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (size == 0)
 		return (NULL);
-	new =  = malloc(sizeof(hash_table_t));
+	new = malloc(sizeof(hash_table_t));
 	if (!new)
 		return (NULL);
 	new->size = size;
