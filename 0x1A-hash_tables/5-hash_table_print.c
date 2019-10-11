ead88bd94abcf4ebdf169005e2daeed95b9b49cf
Juan Sebastian Calle <jusemonca@gmail.com>
2019-10-11 13:36:40 -0500

    [Fix]: Solve space error

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -10,7 +10,7 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *aux;
 
 	if (ht == NULL)
-		printf("{}\n");
+		return ;
 
 	printf("{");
 	while (i < ht->size)
@@ -22,7 +22,7 @@ void hash_table_print(const hash_table_t *ht)
 			{
 				if (f == 1)
 					printf(", ");
-				printf("'%s':'%s'", aux->key, aux->value);
+				printf("'%s': '%s'", aux->key, aux->value);
 				f = 1;
 				aux = aux->next;
 			}
