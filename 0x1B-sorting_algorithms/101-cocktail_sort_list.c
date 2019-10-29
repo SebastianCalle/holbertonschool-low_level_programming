a6c41d07adca3134608ae96fd41ffb78d0c16ac7
Juan Sebastian Calle <jusemonca@gmail.com>
2019-10-29 15:06:19 -0500

    [Fix]: add condition if list is null

--- 0x1B-sorting_algorithms/101-cocktail_sort_list.c
+++ 0x1B-sorting_algorithms/101-cocktail_sort_list.c
@@ -1,5 +1,40 @@
 #include "sort.h"
 /**
+ * swap_nodes_r - change the nodes in the list
+ * @current: current node
+ * @forward: forward node
+ */
+void swap_nodes_r(listint_t *current, listint_t *forward)
+{
+	if (forward->next)
+		forward->next->prev = current;
+	current->next = forward->next;
+	if (current->prev)
+	{
+		current->prev->next = forward;
+	}
+	forward->prev = current->prev;
+	current->prev = forward;
+	forward->next = current;
+
+}
+/**
+ * swap_nodes_l - change the nodes in the list
+ * @current: current node
+ * @forward: forward node
+ */
+void swap_nodes_l(listint_t *current, listint_t *forward)
+{
+	if (forward->prev)
+		forward->prev->next = current;
+	current->prev = forward->prev;
+	current->next->prev = forward;
+	forward->next = current->next;
+	current->next = forward;
+	forward->prev = current;
+
+}
+/**
  * cocktail_sort_list - sort double linked list
  * @list: list to sort
  */
@@ -8,6 +43,8 @@ void cocktail_sort_list(listint_t **list)
 	listint_t *current = *list, *forward, *l;
 	int swap = 1, i, s = 0, size = 0;
 
+	if (list == NULL || (*list) == NULL || (*list)->next == NULL)
+		return;
 	while (current)
 		size++, current = current->next;
 	size--, current = *list, l = *list;
@@ -19,12 +56,11 @@ void cocktail_sort_list(listint_t **list)
 			forward = current->next;
 			if (current->n > forward->n)
 			{
-				if (forward->next)
-					forward->next->prev = current;
-				current->next = forward->next, current->prev->next = forward;
-				forward->prev = current->prev, current->prev = forward;
-				forward->next = current, swap = 1;
-				print_list(l), current = forward; }
+				swap_nodes_r(current, forward), swap = 1;
+				if (!forward->prev)
+					l = forward;
+				print_list(l), current = forward;
+			}
 			current = current->next; }
 		if (!swap)
 			break;
@@ -34,15 +70,13 @@ void cocktail_sort_list(listint_t **list)
 			forward = current->prev;
 			if (forward->n > current->n)
 			{
-				if (forward->prev)
-					forward->prev->next = current;
-				current->prev = forward->prev;
-				current->next->prev = forward, forward->next = current->next;
-				current->next = forward, forward->prev = current, swap = 1;
+				swap_nodes_l(current, forward);
 				if (current->prev == NULL)
 					l = current;
-				print_list(l), current = forward; }
-			current = current->prev; }
+				print_list(l), current = forward, swap = 1;
+			}
+			current = current->prev;
+		}
 		current = current->next, *list = l;
 	}
 }
