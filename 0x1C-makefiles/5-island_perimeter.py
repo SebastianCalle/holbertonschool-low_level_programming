ac38f1dac3c3e370a34a6aaead8ad6485980f6fa
Juan Sebastian Calle <jusemonca@gmail.com>
2019-11-15 22:48:59 -0500

    [Feautre]: function that return the perimeter of the island

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """Return the perimeter of the island grid"""
+    width = 0
+    tmp_w = 0
+    height = 0
+    f = 0
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
+            if grid[x][y] == 1:
+                if f == 0:
+                    height += 1
+                    f = 1
+                tmp_w += 1
+            if width < tmp_w:
+                width = tmp_w
+        f = 0
+        tmp_w = 0
+
+    perimeter = 2 * width + 2 * height
+    return perimeter
+
+    print(height, width)
