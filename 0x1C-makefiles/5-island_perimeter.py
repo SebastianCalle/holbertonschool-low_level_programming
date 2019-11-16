e9d87278fcac00f69c8e0484213dbaeb51d715b8
Juan Sebastian Calle <jusemonca@gmail.com>
2019-11-15 23:18:46 -0500

    [Fix]: solve pep 8 and change perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,21 +3,16 @@
 
 def island_perimeter(grid):
     """Return the perimeter of the island grid"""
-    width = 0
-    tmp_w = 0
-    height = 0
-    f = 0
+    count = 0
     for x in range(len(grid)):
         for y in range(len(grid[x])):
             if grid[x][y] == 1:
-                if f == 0:
-                    height += 1
-                    f = 1
-                tmp_w += 1
-            if width < tmp_w:
-                width = tmp_w
-        f = 0
-        tmp_w = 0
-
-    perimeter = (width + height) * 2
-    return perimeter
+                if x == 0 or grid[x-1][y] == 0:
+                    count += 1
+                if len(grid) - 1 == 0 or grid[x+1][y] == 0:
+                    count += 1
+                if y == 0 or grid[x][y-1] == 0:
+                    count += 1
+                if len(grid[x]) == 0 or grid[x][y+1] == 0:
+                    count += 1
+    return count
