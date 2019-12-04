0b6f95ddc250b75be9c1cdc8a2fd9be8413daeb1
Juan Sebastian Calle <jusemonca@gmail.com>
2019-12-04 15:31:08 -0500

    [Fix]: solve checkers

--- 0x1D-binary_trees/100-binary_trees_ancestor.c
+++ 0x1D-binary_trees/100-binary_trees_ancestor.c
@@ -8,30 +8,43 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 				     const binary_tree_t *second)
 {
-	binary_tree_t *temp = (binary_tree_t *)first,
-		*temp2 = (binary_tree_t *)second;
+	binary_tree_t *temp = (binary_tree_t *)first;
+	binary_tree_t *temp2 = (binary_tree_t *)second;
+	binary_tree_t *parent = NULL;
 
 	if (!first || !second)
 		return (NULL);
-	if (temp->parent == NULL)
-		temp->parent = temp;
-	else if (temp2->parent == NULL)
-		temp->parent = temp;
-	if (first->n == second->parent->n)
-		return (temp2);
-	else if (second->n == first->parent->n)
+	if ((!temp->parent && !temp2->parent) && (temp->n == temp2->n))
 		return (temp);
-
-	while (temp->parent != NULL)
+	if (!temp->parent)
+		parent = temp;
+	else if (!temp2->parent)
+	{
+		parent = temp2;
+		while (parent != NULL)
+		{
+			temp = (binary_tree_t *)first;
+			while (temp->parent != NULL)
+			{
+				if (parent->n == temp2->parent->n)
+					return (parent);
+				temp = temp->parent;
+			}
+			parent = parent->parent;
+		}
+	}
+	else
+		parent = temp->parent;
+	while (parent != NULL)
 	{
 		temp2 = (binary_tree_t *)second;
 		while (temp2->parent != NULL)
 		{
-			if (temp->parent->n == temp2->parent->n)
-				return (temp->parent);
+			if (parent->n == temp2->parent->n)
+				return (parent);
 			temp2 = temp2->parent;
 		}
-		temp2 = temp2->parent;
+		parent = parent->parent;
 	}
 	return (NULL);
 }
