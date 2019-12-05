bf24f24e983727c9448ce27be81254bb27fdd1c3
Juan Sebastian Calle <jusemonca@gmail.com>
2019-12-04 22:29:25 -0500

    [Feature]: update files

--- 0x1D-binary_trees/2-binary_tree_insert_right.c
+++ 0x1D-binary_trees/2-binary_tree_insert_right.c
@@ -12,24 +12,25 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	binary_tree_t *node = NULL, *temp = NULL;
 
 	if (!parent)
+		return (NULL);
+	if (!parent->right)
+	{
 		node = binary_tree_node(parent, value);
+		if (!node)
+			return (NULL);
+		parent->right = node;
+		node->parent = parent;
+	}
 	else
 	{
-		if (!parent->right)
-		{
-			node = binary_tree_node(parent, value);
-			parent->right = node;
-			node->parent = parent;
-		}
-		else
-		{
-			node = binary_tree_node(parent, value);
-			temp = parent->right;
-			parent->right = node;
-			node->parent = parent;
-			node->right = temp;
-			temp->parent = node;
-		}
+		node = binary_tree_node(parent, value);
+		if (!node)
+			return (NULL);
+		temp = parent->right;
+		parent->right = node;
+		node->parent = parent;
+		node->right = temp;
+		temp->parent = node;
 	}
 	return (node);
 }
--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -21,6 +21,7 @@ typedef struct binary_tree_s
 	struct binary_tree_s *right;
 } binary_tree_t;
 
+typedef struct binary_tree_s bst_t;
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
@@ -49,5 +50,10 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_root(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+int isBST(const binary_tree_t *tree, int min, int max);
+int binary_tree_is_bst(const binary_tree_t *tree);
+bst_t *bst_insert(bst_t **tree, int value);
+bst_t *array_to_bst(int *array, size_t size);
+bst_t *bst_search(const bst_t *tree, int value);
 int binary_tree_is_avl(const binary_tree_t *tree);
 #endif /* _BINARY_TREES_H_ */
