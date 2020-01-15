8e943551e94c76d78a2a83646d4584e40afb87e0
Juan Sebastian Montealegre <jusemonca@gmail.com>
2020-01-15 22:14:43 +0000

    [Feature]: Create funtion to print

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,25 +1,5 @@
 #include "search_algos.h"
 /**
- * print_array - print array
- * @i: initial position
- * @f: final position
- * @array: array to print
- */
-void print_array(size_t i, size_t f, int *array)
-{
-	printf("Searching in array: ");
-	while (i <= f)
-	{
-		printf("%d", array[i]);
-		if (i != f)
-			printf(", ");
-		i++;
-	}
-	printf("\n");
-
-}
-
-/**
  * binary_search - Function that searches for a value in a array
  * @array: pointer to first element to array
  * @size: size of array
@@ -28,29 +8,28 @@ void print_array(size_t i, size_t f, int *array)
  */
 int binary_search(int *array, size_t size, int value)
 {
-	int l, r, m, i, f;
+	int l, r, m, i;
 
 	if (array == NULL || size == 0)
 		return (-1);
-	l = i = 0;
-	r = (int)size - 1;
+	l = 0;
+	r = size - 1;
 
 	while (l <= r)
 	{
-		f = r;
-		print_array(i, f, array);
+		printf("Seaching in array: ");
+		for (i = l; i <= r; i++)
+		{
+			printf("%d", array[i]);
+			if (i != r)
+				printf(", ");
+		}
+		printf("\n");
 		m = (l + r) / 2;
 		if (array[m] < value)
-		{
 			l = m + 1;
-			i = l;
-		}
 		else if (array[m] > value)
-		{
 			r = m - 1;
-			i = 0;
-
-		}
 		else
 			return (m);
 
